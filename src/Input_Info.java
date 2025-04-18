0b0c31ff3c3e8af58720f61908ae7c8fdc0d2861
KaizerVolfrang <vladchernov12@yandex.ru>
2025-04-19 00:56:20 +0300

    second commit

---
+++ src/Input_Info.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Input_Info {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите ваше имя: "); // Запрос имени у пользователя
+        String name = scanner.nextLine(); // Считывание строки
+
+        System.out.print("Введите ваш возраст: "); // Запрос возраста у пользователя
+        int age = scanner.nextInt(); // Считывание целого числа
+
+        // Вывод приветствия с полученной информацией
+        System.out.println("Привет, " + name + "! Вам " + age + " лет.");
+
+    }
+}
